6d1140cd60d9a7a4e120be5f26c041b506d7cdb4
ActiveApps <douglas.luzinda.ot@gmail.com>
2023-08-29 14:25:49 +0300

    add invoicing

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -22,3 +22,20 @@ CREATE TABLE treatments
 	type varchar(100),
 	name varchar(100)
 );
+
+CREATE TABLE invoices(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	total_amount DECIMAL,
+	generated_at TIMESTAMP,
+	paid_at TIMESTAMP,
+	medical_history_id INT,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE history_and_treatments (
+	medical_history_id INT, 
+	treatment_id INT, 
+	FOREIGN KEY (medical_history_id)REFERENCES medical_histories(id), 
+	FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
+
