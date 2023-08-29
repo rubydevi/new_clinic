1ea9f543b9242ccd72eb9f3645ca4cbcef4629ef
iamrubydevi <ariesabyss@gmail.com>
2023-08-29 17:13:20 +0530

    Crate indices for foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -32,6 +32,15 @@ CREATE TABLE invoices(
 	PRIMARY KEY(id)
 );
 
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  unit_price DECIMAL(10,2),
+  quantity INT,
+  total_price DECIMAL(10,2),
+  invoice_id INT REFERENCES invoices (id),
+  treatment_id INT REFERENCES treatments(id)
+);
+
 CREATE TABLE history_and_treatments (
 	medical_history_id INT, 
 	treatment_id INT, 
@@ -39,3 +48,16 @@ CREATE TABLE history_and_treatments (
 	FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
 
+-- CREATE FK INDICES
+
+CREATE INDEX idx_p_id ON medical_histories (patient_id);
+
+CREATE INDEX idx_in_mh_id ON invoices (medical_history_id);
+
+CREATE INDEX idx_in_id ON invoice_items (invoice_id);
+
+CREATE INDEX idx_int_tr_id ON invoice_items (treatment_id);
+
+CREATE INDEX idx_ht_mh_id ON history_and_treatments (medical_history_id);
+
+CREATE INDEX idx_ht_tr_id ON history_and_treatments (treatment_id);
